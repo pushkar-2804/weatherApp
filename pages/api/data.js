3513916deeff09a2792a191bf32d08ce6873d03c
pushkar-28 <kharepushkar2804@gmail.com>
2023-06-27 19:44:42 +0530

    responsive

---
+++ pages/api/data.js
@@ -0,0 +1,23 @@
+// export default async function handler(req, res) {
+//   const { cityInput } = req.body;
+//   const getWeatherData = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
+//   );
+//   const data = await getWeatherData.json();
+//   res.status(200).json(data);
+// }
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { cityInput } = req.body;
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+}
